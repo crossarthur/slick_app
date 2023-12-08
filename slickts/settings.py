be06399f850c02372cffad2fcfe28a15196388a1
arthur <arthurnwolisa@gmail.com>
2023-12-08 08:13:05 +0100

    mysql

--- slickts/settings.py
+++ slickts/settings.py
@@ -91,26 +91,26 @@ WSGI_APPLICATION = 'slickts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
-'''DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'slickts',
+        'USER': 'root',
         'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
 
     }
 
-}'''
+}
 
 
 # Password validation
